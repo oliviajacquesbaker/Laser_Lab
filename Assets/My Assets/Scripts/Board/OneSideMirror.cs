5c4637e4d8b4f1f017375533a4b289653a66d45f
pip <piptastc@gmail.com>
2020-02-18 14:34:15 -0500

    OneSideMirror OnLaserHit() rotation still needs to be done, functions functions have to be checked for correctness;;

---
+++ Assets/My Assets/Scripts/Board/OneSideMirror.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneSideMirror : BoardObject
+{
+    //rotation of zero is a mirror in this position: \
+    // with the facing direction (the mirrored side) facing northeast
+    public int rotation = 0;
+    private Laser.Direction getNewDirection(Laser laser)
+    {
+        Laser.Direction dir = laser.direction;
+        if (rotation == 0)
+        {
+            if (dir == Laser.Direction.DOWN)
+                return Laser.Direction.RIGHT;
+            else if (dir == Laser.Direction.LEFT)
+                return Laser.Direction.UP;
+        }
+        else if (rotation == 1)
+        {
+            if (dir == Laser.Direction.DOWN)
+                return Laser.Direction.LEFT;
+            else if (dir == Laser.Direction.RIGHT)
+                return Laser.Direction.UP;
+        }
+        else if (rotation == 2)
+        {
+            if (dir == Laser.Direction.UP)
+                return Laser.Direction.LEFT;
+            else if (dir == Laser.Direction.RIGHT)
+                return Laser.Direction.DOWN;
+        }
+        else
+        {
+            if (dir == Laser.Direction.UP)
+                return Laser.Direction.RIGHT;
+            else if (dir == Laser.Direction.LEFT)
+                return Laser.Direction.DOWN;
+        }
+
+        return Laser.Direction.UP;
+    }
+    public override Laser[] OnLaserHit(Laser laser)
+    {
+        //assumes that the facing direction is the side with the mirror
+
+        Vector3 laserOrigin = new Vector3(laser.origin.x, laser.origin.y);
+        Vector3 mirrorLaserDir = transform.position - laserOrigin;
+        float dotProduct = Vector3.Dot(mirrorLaserDir, transform.forward);
+
+        if (dotProduct > 0)
+        {
+            Vector2Int newOrigin = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+            Laser.Direction newDirection = getNewDirection(laser);
+            Laser newLaser = new Laser(newOrigin, newDirection, laser.red, laser.green, laser.blue);
+            Laser[] returning = new Laser[1];
+            returning[0] = newLaser;
+            return returning;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+}
+
