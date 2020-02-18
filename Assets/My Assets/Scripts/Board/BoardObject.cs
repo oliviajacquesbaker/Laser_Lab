dda95d4222d60bea0def4866930ebefbb2b45574
Mantis <mjbgtaad56@gmail.com>
2020-02-18 11:18:44 -0500

    Created BoardObject
    Class incomplete. Simple functionality available.

---
+++ Assets/My Assets/Scripts/Board/BoardObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BoardObject : MonoBehaviour, ILaserTarget
+{
+    public int Rotation { get; protected set; }
+    public abstract Laser[] OnLaserHit(Laser laser);
+}
