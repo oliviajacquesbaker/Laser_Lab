1906485b593415151254aa41c2fb2f9106bc2544
Mantis <mjbgtaad56@gmail.com>
2020-02-26 08:17:43 -0500

    Created Level and Level Set objects
    Organized BoardObject scripts

--- Assets/My Assets/Scripts/Game/Level.cs
+++ Assets/My Assets/Scripts/Game/Level.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Game/Level")]
 public class Level : ScriptableObject
 {
     public Board board;
---
+++ Assets/My Assets/Scripts/Game/LevelSet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Level Set")]
+public class LevelSet : ScriptableObject
+{
+    public Level[] levels;
+}
