b770f7e0471eea848b39fcd64a41814f00d48b25
Mantis <mjbgtaad56@gmail.com>
2020-04-04 00:15:27 -0400

    Added reset progress option

---
+++ Assets/My Assets/Scripts/Menu/Options.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mainMenu;
+    [SerializeField]
+    GameObject optionsMenu;
+
+    [SerializeField]
+    Text resetButtonText;
+
+    bool resetConfirm = false;
+    bool resetDone = false;
+
+    public void showOptions()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void hideOptions()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+
+        resetConfirm = false;
+        resetDone = false;
+        resetButtonText.text = "RESET PROGRESS";
+    }
+
+    public void clickReset()
+    {
+        if (!resetConfirm)
+        {
+            resetConfirm = true;
+            resetButtonText.text = "CONFIRM RESET?";
+        } else
+        {
+            if (!resetDone)
+            {
+                PlayerPrefs.DeleteAll();
+                resetDone = true;
+                resetButtonText.text = "RESET COMPLETE";
+            }
+        }
+    }
+}
