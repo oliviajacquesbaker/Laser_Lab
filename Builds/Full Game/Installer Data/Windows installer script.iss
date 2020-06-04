3b954e3088a690035e6bb3982e3528696e753319
Mantis <mjbgtaad56@gmail.com>
2020-06-04 01:33:51 -0400

    Built update 1.2
    Added audio sliders

--- Builds/Full Game/Installer Data/Windows installer script.iss
+++ Builds/Full Game/Installer Data/Windows installer script.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Laser Lab"
-#define MyAppVersion "1.1"
+#define MyAppVersion "1.2"
 #define MyAppPublisher "Mantis Studios"
 #define MyAppExeName "Laser Lab.exe"
 
---
+++ Laser Lab Unity Project/Assets/My Assets/Scripts/Audio/MusicSlider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(Slider))]
+public class MusicSlider : MonoBehaviour
+{
+    public string prefName;
+    public AudioMixer mixer;
+    public string mixerParameterName;
+
+    Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        slider.onValueChanged.AddListener(onSlide);
+        if (PlayerPrefs.HasKey(prefName))
+            slider.value = PlayerPrefs.GetFloat(prefName);
+        else
+            PlayerPrefs.SetFloat(prefName, 1);
+        mixer.SetFloat(mixerParameterName, 20 * Mathf.Log10(slider.value));
+    }
+
+    public void onSlide(float value)
+    {
+        PlayerPrefs.SetFloat(prefName, value);
+        mixer.SetFloat(mixerParameterName, 20 * Mathf.Log10(value));
+    }
+}
